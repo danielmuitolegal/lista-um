0c0a80e716e002708e93ce6a7a5dbd88a099679c
danielmuitolegal <daniel_schutz@estudante.sesisenai.org.br>
2025-08-25 09:58:07 -0300

    3 pronta

--- 3/index.php
+++ 3/index.php
@@ -4,27 +4,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercício 01 - Par ou Ímpar</title>
+    <title>Exercício 03 - Positivo ou Negativo</title>
 </head>
 
 <body>
     <form method="POST" action="">
         <label for="numero">Digite um número:</label>
         <input type="number" id="numero" name="numero" required>
-        <button type="submit" name="verificar_par_impar">Verificar</button>
+        <button type="submit" name="verificar_p_n">Verificar</button>
     </form>
 
     <?php
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        if (isset($_POST['verificar_par_impar'])) {
+        if (isset($_POST['verificar_p_n'])) {
             $numero = filter_var($_POST['numero'], FILTER_VALIDATE_INT);
             if ($numero === false) {
                 echo "Número inválido!";
             } else {
-                if ($numero % 2 == 0) {
-                    echo "O número $numero é <strong>par</strong>.";
+                if ($numero > 0) {
+                    echo "O número $numero é <strong>positivo</strong>.";
+                } elseif ($numero < 0) {
+                    echo "O número $numero é <strong>negativo</strong>.";
                 } else {
-                    echo "O número $numero é <strong>ímpar</strong>.";
+                    echo "O número $numero é <strong>zero</strong>.";
                 }
             }
         }
